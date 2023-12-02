ac8b52bd0fbcd6fc7cc5a338b4464bab7813b09e
danielrobl3s <danielrobles200217@gmail.com>
2023-12-01 23:12:06 -0600

    nav bar con bottomnavbaritems

---
+++ lib/pages/paginaHome.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class paginaHome extends StatelessWidget {
+  const paginaHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Este es el home",
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+}
---
+++ lib/pages/paginaUsers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class paginaUsers extends StatelessWidget {
+  const paginaUsers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Esto es users",
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+}
