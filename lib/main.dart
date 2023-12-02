0e85f8af1f89c344f8be0d9056f1990be67ee3b6
danielrobl3s <danielrobles200217@gmail.com>
2023-12-01 22:13:08 -0600

    login props en flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const Miapp());
@@ -24,25 +26,78 @@ class Inicio extends StatefulWidget {
 class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("workout!"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            var t = DateTime.now();
-            print(t);
-          },
-          child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-            Icon(Icons.access_time),
-            SizedBox(
-              height: 7,
-            ),
-            Text("Now")
-          ]),
-        ),
-      ),
-    );
+    return Scaffold(body: cuerpo());
   }
+}
+
+//separated widgets
+Widget cuerpo() {
+  return Container(
+    decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage(
+                "https://e1.pxfuel.com/desktop-wallpaper/646/153/desktop-wallpaper-dumbbells-fitnesss-by-iammiti-gym-mobile.jpg"),
+            fit: BoxFit.cover)),
+    child: Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        nombre(),
+        SizedBox(
+          height: 10,
+        ),
+        campousuario(),
+        SizedBox(
+          height: 10,
+        ),
+        campoContrasena(),
+        SizedBox(
+          height: 30,
+        ),
+        botonEntrar(),
+      ],
+    )),
+  );
+}
+
+Widget nombre() {
+  return Text(
+    "Sign in",
+    style: TextStyle(
+        color: Colors.white, fontSize: 35.0, fontWeight: FontWeight.bold),
+  );
+}
+
+Widget campousuario() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+    child: TextField(
+      decoration: InputDecoration(
+          hintText: "User", fillColor: Colors.white, filled: true),
+    ),
+  );
+}
+
+Widget campoContrasena() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          hintText: "Password", fillColor: Colors.white, filled: true),
+    ),
+  );
+}
+
+Widget botonEntrar() {
+  return TextButton(
+    style: TextButton.styleFrom(
+        backgroundColor: Colors.blueGrey,
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 17)),
+    child: Text(
+      "Enter",
+      style: TextStyle(color: Colors.black, fontSize: 20),
+    ),
+    onPressed: () {},
+  );
 }
